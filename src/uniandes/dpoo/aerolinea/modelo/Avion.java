a97b201ab23b4737f7c9fab363cd1360f1c28ca3
Catalina Santamaria <Catalina Santamaria@carmenn>
2024-02-22 20:52:11 -0500

    Avances- clase avion

---
+++ src/uniandes/dpoo/aerolinea/modelo/Avion.java
@@ -0,0 +1,17 @@
+package uniandes.dpoo.aerolinea.modelo;
+
+public class Avion {
+
+	private String nombre;
+	private int capacidad;
+	
+	// metodos
+	public String getNombre() {
+		return nombre;
+	}
+	public int getCapacidad() {
+		return capacidad;
+	} 
+	
+	
+}
